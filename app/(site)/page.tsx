5bffd7dba5b583bbf15d900afd4f130b89a00fd0
Mohamed Sharif <Mohamed8sharif@gmail.com>
2024-09-16 15:00:13 -0500

    Add a liked song button

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,4 +1,5 @@
 import Header from "@/components/Header";
+import ListItem from "@/components/ListItem";
 
 export default function Home() {
   return (
@@ -11,9 +12,30 @@ export default function Home() {
     overflow-y-auto">
       <Header>
         <div className="mb-2">
-          <h1 className="">
+          <h1 
+          className="
+          text-white
+          text-3xl
+          font-semibold
+          ">
             Welcome back
           </h1>
+          <div
+            className="
+            grid
+            grid-cols-1
+            sm:grid-cols-2
+            xl:grid-cols-3
+            2xl:grid-cols-4
+            gap-3
+            mt-4
+            ">
+              <ListItem 
+              image="/images/liked.png"
+              name="Liked songs"
+              href="liked"
+              />
+          </div>
         </div>
       </Header>
     </div>
