ee8f3986884bdb0ac4e87c3bcf2eb037faacd34c
Mohamed Sharif <Mohamed8sharif@gmail.com>
2024-09-15 19:11:59 -0500

    Create icons for side bar as well as create the general layout

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -5,6 +5,8 @@ import { useMemo } from "react";
 import { BiSearch } from "react-icons/bi";
 import { HiHome } from "react-icons/hi";
 import Box from "./Box";
+import SidebarItem from "./SidebarItem";
+import Library from "./Library";
 
 interface SidebarProps {
     children: React.ReactNode;
@@ -45,9 +47,30 @@ const Sidebar: React.FC<SidebarProps> = ({
                 "
             >
                 <Box>
-                    Sidebar Navigation
+                    <div
+                     className="
+                     flex
+                     flex-col
+                     gap-y-2
+                     px-5
+                     py-4
+                     "
+                     >
+                        {routes.map((item) => (
+                            <SidebarItem
+                            key={item.label}
+                            {...item}
+                            />
+                        ))}
+                     </div>
+                </Box>
+                <Box className="overflow-y-auto h-full">
+                    <Library></Library>
                 </Box>
             </div>
+            <main className="h-full flex-1 overflow-y-auto py-2">
+                {children}
+            </main>
         </div>
     );
 }
