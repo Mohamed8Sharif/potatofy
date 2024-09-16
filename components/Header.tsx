082212ecf8d600d631757670ac4aa7e184aa9b26
Mohamed Sharif <Mohamed8sharif@gmail.com>
2024-09-16 13:55:02 -0500

    Add home button and search button for mobile view

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import { RxCaretLeft } from "react-icons/rx";
+import { BiSearch } from "react-icons/bi";
+import { HiHome } from "react-icons/hi";
+import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
 import { twMerge } from "tailwind-merge";
 
 interface HeaderProps {
@@ -41,9 +43,73 @@ const Header: React.FC<HeaderProps> = ({
                 gap-x-2
                 items-center
                 ">
-                    <button>
-                        <RxCaretLeft />
+                    <button
+                        onClick={() => router.back()}
+                        className="
+                        rounded-full
+                        bg-black
+                        flex
+                        items-center
+                        justify-center
+                        hover:opacity-75
+                        transition">
+                        <RxCaretLeft className="text-white" size={35}/>
                     </button>
+                    <button
+                        onClick={() => router.forward()}
+                        className="
+                        rounded-full
+                        bg-black
+                        flex
+                        items-center
+                        justify-center
+                        hover:opacity-75
+                        transition">
+                        <RxCaretRight className="text-white" size={35}/>
+                    </button>
+                </div>
+                <div className="flex md:hidden gap-x-2 items-center">
+                    <button 
+                        className="
+                            rounded-full
+                            p-2
+                            bg-white
+                            flex
+                            items-center
+                            justify-center
+                            hover:opacity-75
+                            transition
+                        ">
+                        <HiHome className="text-black" size={20}/>
+                    </button>
+
+                    <button 
+                        className="
+                            rounded-full
+                            p-2
+                            bg-white
+                            flex
+                            items-center
+                            justify-center
+                            hover:opacity-75
+                            transition
+                        ">
+                        <BiSearch className="text-black" size={20}/>
+                    </button>
+                </div>
+
+                <div
+                className="
+                flex
+                justify-between
+                items-center
+                gap-x-4">
+
+                    <>
+                        <div>
+                            <Button />
+                        </div>
+                    </>
                 </div>
             </div>
         </div>
