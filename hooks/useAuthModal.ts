3bed109bf24f3cc1cdf2537cf057730b6c700bf0
Mohamed Sharif <Mohamed8sharif@gmail.com>
2024-09-16 22:55:35 -0500

    Fix bug where you could not close sign in auth modal

--- hooks/useAuthModal.ts
+++ hooks/useAuthModal.ts
@@ -8,8 +8,8 @@ interface AuthModalStore {
 
 const useAuthModal = create<AuthModalStore>((set) => ({
     isOpen: false,
-    onOpen: () => set({isOpen: true}),
-    onClose: () => set({isOpen: false}),
+    onOpen: () => set({ isOpen: true }),
+    onClose: () => set({ isOpen: false }),
 }));
 
 export default useAuthModal;
