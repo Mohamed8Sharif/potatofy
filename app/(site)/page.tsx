973cac9e1140ffcbd8589268727b17c9805ba2a8
Mohamed Sharif <Mohamed8sharif@gmail.com>
2024-09-17 13:38:09 -0500

    create a hook to load images

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,7 +1,13 @@
 import Header from "@/components/Header";
 import ListItem from "@/components/ListItem";
+import getSongs from "@/actions/getSongs"
+import PageContent from "@/components/PageContent";
 
-export default function Home() {
+export const revalidate = 0;
+
+export default async function Home() {
+  const songs = await getSongs();
+
   return (
     <div className="
     bg-neutral-900
@@ -45,9 +51,7 @@ export default function Home() {
           </h1>
         </div>
 
-        <div>
-          List of songs!
-        </div>
+        <PageContent songs={songs}/>
       </div>
     </div>
   )
