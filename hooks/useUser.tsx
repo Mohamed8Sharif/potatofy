b469d1df078d4dbbaafab05a1794664e0a035950
Mohamed Sharif <Mohamed8sharif@gmail.com>
2024-09-16 19:46:43 -0500

    create provider

--- hooks/useUser.tsx
+++ hooks/useUser.tsx
@@ -30,10 +30,10 @@ export const MyUserContextProvider = (props: Props) => {
     const accessToken = session?.access_token ?? null;
     const [isLoadingData, setIsLoadingData] = useState(false);
     const [userDetails, setUserDetails] = useState<UserDetails | null>(null);
-    const [subscription, setSubsciption] = useState<Subscription | null>(null);
+    const [subscription, setSubscription] = useState<Subscription | null>(null);
 
     const getUserDetails = () => supabase.from('users').select('*').single();
-    const getSubsciption = () => supabase.from('subscriptions')
+    const getSubscription = () => supabase.from('subscriptions')
                                     .select('*, prices(*,products(*))')
                                     .in('status', ['trialing', 'active'])
                                     .single();
@@ -42,8 +42,35 @@ export const MyUserContextProvider = (props: Props) => {
         if (user && !isLoadingData && !userDetails && !subscription){
             setIsLoadingData(true);
 
-            Promise.allSettled([])
-        }
-    }, []);
+            Promise.allSettled([getUserDetails(), getSubscription()]).then(
+                (results) => {
+                    const userDetailsPromise = results[0];
+                    const subscriptionPromise = results[1];
 
-}
+                    if (userDetailsPromise.status === "fulfilled") {
+                        setUserDetails(userDetailsPromise.value.data as UserDetails);
+                    }
+
+                    if (subscriptionPromise.status === "fulfilled") {
+                        setSubscription(subscriptionPromise.value.data as Subscription);
+                    }
+
+                    setIsLoadingData(false);
+                }
+            );
+        } else if (!user && !isLoadingUser && !isLoadingData) {
+            setUserDetails(null);
+            setSubscription(null);
+        }
+    }, [user, isLoadingUser]);
+
+    const value = {
+        accessToken,
+        user,
+        userDetails,
+        isLoading: isLoadingUser || isLoadingData,
+        subscription
+    };
+
+    return <UserContext.Provider value={value} {...props}/>
+};
