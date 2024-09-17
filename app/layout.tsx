fee7fe6963e2b7844c51ac23edd5000749300166
Mohamed Sharif <Mohamed8sharif@gmail.com>
2024-09-16 19:21:32 -0500

    install stuff for supabase database

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Figtree } from 'next/font/google'
 import Sidebar from "@/components/Sidebar";
+import SupabaseProvider from "@/providers/SupabaseProvider";
 
 const font = Figtree({subsets: ['latin']})
 
@@ -20,9 +21,11 @@ export default function RootLayout({
       <body
         className={font.className}
       >
-        <Sidebar>
-          {children}
-        </Sidebar>
+        <SupabaseProvider>
+          <Sidebar>
+            {children}
+          </Sidebar>
+        </SupabaseProvider>
       </body>
     </html>
   );
--- providers/SupabaseProvider.tsx
+++ providers/SupabaseProvider.tsx
@@ -1,6 +1,9 @@
 "use client";
 
 import { Database } from "@/types_db";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
+import { useState } from "react";
 
 interface SupabaseProviderProps { 
     children: React.ReactNode;
@@ -9,5 +12,15 @@ interface SupabaseProviderProps {
 const SupabaseProvider: React.FC<SupabaseProviderProps> = ({
     children
 }) => {
-    
+    const [supabaseClient] = useState(() => 
+        createClientComponentClient<Database>()
+    );
+
+    return (
+        <SessionContextProvider supabaseClient={supabaseClient}>
+            {children}
+        </SessionContextProvider>
+    )
 }
+
+export default SupabaseProvider;
