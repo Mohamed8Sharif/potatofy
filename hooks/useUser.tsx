03855b682fa547edc9746088f1d8f3a56a88dcc5
Mohamed Sharif <Mohamed8sharif@gmail.com>
2024-09-16 19:51:48 -0500

    Create another user provider

--- hooks/useUser.tsx
+++ hooks/useUser.tsx
@@ -1,7 +1,7 @@
 import { UserDetails, Subscription } from "@/types";
 import { User } from "@supabase/auth-helpers-nextjs";
 import { useSessionContext, useUser as useSupaUser } from "@supabase/auth-helpers-react";
-import { createContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 type UserContextType = {
     accessToken: string | null;
@@ -73,4 +73,13 @@ export const MyUserContextProvider = (props: Props) => {
     };
 
     return <UserContext.Provider value={value} {...props}/>
+};
+
+export const useUser = () => {
+    const context = useContext(UserContext);
+    if (context === undefined) {
+        throw new Error('useUser must be used within a MyUserContextProvider');
+    }
+
+    return context;
 };
