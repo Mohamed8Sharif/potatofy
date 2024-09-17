403cddc3bdee42f0be4b2e5992a2e561a1ae297a
Mohamed Sharif <Mohamed8sharif@gmail.com>
2024-09-17 00:28:06 -0500

    create start of form to upload songs

--- components/UploadModal.tsx
+++ components/UploadModal.tsx
@@ -2,24 +2,54 @@
 
 import useUploadModal from "@/hooks/useUploadModal";
 import Modal from "./Modal"
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import { useState } from "react";
+import Input from "./Input";
 
 const UploadModal = () => {
+    const [isLoading, setIsLoading] = useState();
     const uploadModal = useUploadModal();
+
+    const {
+        register,
+        handleSubmit,
+        reset
+    } = useForm<FieldValues>({
+        defaultValues:{
+            author: '',
+            title: '',
+            song: null,
+            image: null,
+        }
+    })
 
     const onChange = (open: boolean) => {
         if(!open) {
-            //reset the form
+            reset();
             uploadModal.onClose();
         }
     }
 
+    const onSubmit: SubmitHandler<FieldValues> = async (values) => {
+        // Upload to supabase
+    }
+
     return (
         <Modal
-        title="Upload modal title"
-        description="Upload modal description"
+        title="Add a song"
+        description="Upload an mp3 file"
         isOpen={uploadModal.isOpen}
         onChange={onChange}>
-            Upload Content
+            <form
+            onSubmit={handleSubmit(onSubmit)}>
+
+                <Input 
+                    id="title"
+                    disabled={isLoading}
+                    {...register('title', {required: true})}
+                    placeholder="Song title"
+                />
+            </form>
         </Modal>
     );
 }
