6886092cfa2c31e8608461c0215165feda64f0c2
Mohamed Sharif <Mohamed8sharif@gmail.com>
2024-09-16 21:14:29 -0500

    create modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,28 +3,33 @@ import "./globals.css";
 import { Figtree } from 'next/font/google'
 import Sidebar from "@/components/Sidebar";
 import SupabaseProvider from "@/providers/SupabaseProvider";
+import UserProvider from "@/providers/UserProvider";
+import ModalProvider from "@/providers/ModalProvider";
 
 const font = Figtree({subsets: ['latin']})
 
 export const metadata: Metadata = {
-  title: "Spotify Clone",
+  title: "Potatofy",
   description: "Listen to music!",
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="en">
       <body
         className={font.className}
       >
         <SupabaseProvider>
-          <Sidebar>
-            {children}
-          </Sidebar>
+          <UserProvider>
+            <ModalProvider/>
+            <Sidebar>
+              {children}
+            </Sidebar>
+          </UserProvider>
         </SupabaseProvider>
       </body>
     </html>
